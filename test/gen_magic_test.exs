ea78907efa01bbcb343ae8f93ad51eb050937813
James Every <devstopfix@gmail.com>
2019-07-03 21:43:01 +0100

    test: makefile

--- test/gen_magic_test.exs
+++ test/gen_magic_test.exs
@@ -2,7 +2,17 @@ defmodule GenMagicTest do
   use ExUnit.Case
   doctest GenMagic
 
-  test "greets the world" do
-    assert GenMagic.hello() == :world
+  alias GenMagic.ApprenticeServer, as: Magic
+
+  setup_all do
+    {:ok, pid} = Magic.start_link()
+    {:ok, %{pid: pid}}
+  end
+
+  test "Makefile is text file", %{pid: pid} do
+    path = File.cwd!() |> Path.join("Makefile")
+
+    assert {:ok, [mime_type: "text/x-makefile", encoding: _, content: _]} =
+             GenServer.call(pid, {:perform, path})
   end
 end
