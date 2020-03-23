326bac29335f511be4f5a55a3cf58c179c882c3e
James Every <devstopfix@gmail.com>
2020-03-23 15:10:08 +0000

    OTP 22 compatible with Elixir 1.7-1.10

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule GenMagic.MixProject do
     [
       app: :gen_magic,
       version: "0.20.83",
-      elixir: "~> 1.6",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       compilers: [:elixir_make] ++ Mix.compilers(),
       deps: deps()
