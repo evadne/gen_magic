19ebf1cd884118ce7eaba649317ae74f381f1de8
Evadne Wu <ev@radi.ws>
2021-03-10 20:09:00 +0000

    → 1.0.2

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule GenMagic.MixProject do
   def project do
     [
       app: :gen_magic,
-      version: "1.0.1",
+      version: "1.0.2",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
