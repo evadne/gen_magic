8deef6e892b75b695f507ae138e43a21b9918846
Ruslan Levond <ruslan@managebac.com>
2023-06-05 20:12:35 +0100

    Upgraded elixir_make to v0.7.7

--- mix.exs
+++ mix.exs
@@ -43,7 +43,7 @@ defmodule GenMagic.MixProject do
       {:credo, "~> 1.5.5", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.1.0", only: :dev, runtime: false},
       {:ex_doc, "~> 0.23.0", only: :dev, runtime: false},
-      {:elixir_make, "~> 0.6.2", runtime: false},
+      {:elixir_make, "~> 0.7.7", runtime: false},
       {:nimble_pool, "~> 0.2.4", only: [:dev, :test]},
       {:poolboy, "~> 1.5.2", only: [:dev, :test]}
     ]
